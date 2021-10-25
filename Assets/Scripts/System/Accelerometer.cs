3a6021369913778d194ed40dfe4efd0b7639e223
Dmitry <romanov.ds35@gmail.com>
2021-10-25 21:29:29 +0300

    parallax update

--- Assets/Scripts/System/Accelerometer.cs
+++ Assets/Scripts/System/Accelerometer.cs
@@ -23,8 +23,8 @@ public class Accelerometer : MonoBehaviour
     private float _forceX;
     private float _forceZ;
 
-    private float _defaultAngleX;
-    private float _defaultAngleY;
+    private float _angleX;
+    private float _angleY;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +32,6 @@ public class Accelerometer : MonoBehaviour
         _defaultX = transform.position.x;
         _defaultY = transform.position.y;
         _defaultZ = transform.position.z;
-
-        _defaultAngleX = GetAngleX();
-        _defaultAngleY = GetAngleY();
     }
 
     // Update is called once per frame
@@ -46,10 +43,10 @@ public class Accelerometer : MonoBehaviour
             Vector3 newPos = transform.position;
 
             // force
-            float forceX = _forceX + (Input.acceleration.x / 10);
+            float forceX = _forceX - (Input.acceleration.x / 10);
             _forceX = forceX < 0 ? Mathf.Max(-1, forceX) : Mathf.Min(1, forceX);
 
-            float forceZ = _forceZ + (Input.acceleration.z / 10);
+            float forceZ = _forceZ - (Input.acceleration.z / 10);
             _forceZ = forceZ < 0 ? Mathf.Max(-1, forceZ) : Mathf.Min(1, forceZ);
 
             // delta
@@ -74,16 +71,20 @@ public class Accelerometer : MonoBehaviour
 
             if (forceZ > 0)
             {
-
+                speedZ = transform.position.z > _defaultZ
+                    ? sideSpeed * (maxShift - _shiftZ) / maxShift
+                    : sideSpeed;
             }
             else
             {
-
+                speedZ = transform.position.z < _defaultZ
+                    ? sideSpeed * (maxShift - _shiftZ) / maxShift
+                    : sideSpeed;
             }
 
             // move
             newPos.x = Mathf.Lerp(newPos.x, _defaultX + deltaX, Time.deltaTime * Mathf.Max(speedX, 0.1f));
-            newPos.z = Mathf.Lerp(newPos.z, _defaultZ + deltaZ, Time.deltaTime * sideSpeed);
+            newPos.z = Mathf.Lerp(newPos.z, _defaultZ + deltaZ, Time.deltaTime * Mathf.Max(speedZ, 0.1f));
 
             transform.position = newPos;
         }
@@ -92,27 +93,16 @@ public class Accelerometer : MonoBehaviour
         // rotation
         if (maxAngle != 0)
         {
-            float deltaAngleX = maxAngle * Mathf.Abs(Input.acceleration.x);
-            float resultAngleX = _defaultAngleX + deltaAngleX;
+            float deltaAngleX = maxAngle * Input.acceleration.x;
+            float _rotateX = deltaAngleX - _angleX;
+            _angleX = deltaAngleX;
 
-            float deltaAngleY = maxAngle * Mathf.Abs(Input.acceleration.y);
-            float resultAngleY = _defaultAngleY + deltaAngleY;
+            //float deltaAngleY = maxAngle * Mathf.Abs((Input.acceleration.y + Input.acceleration.z) / 2);
+            //float _rotateY = deltaAngleY - _angleY;
+            //_angleY = deltaAngleY;
 
-            int kx = Input.acceleration.x > 0 ? 1 : -1;
-            int ky = Input.acceleration.y > 0 ? 1 : -1;
-
-            transform.Rotate(Vector3.up, kx * (resultAngleX - GetAngleX()));
-            // transform.Rotate(Vector3.left, ky * (resultAngleY - GetAngleY())); - doesn't work
+            transform.Rotate(Vector3.up, _rotateX);
+            // transform.Rotate(Vector3.left, _rotateY);
         }
-    }
-
-    private float GetAngleX()
-    {
-        return transform.rotation.eulerAngles.x;
-    }
-
-    private float GetAngleY()
-    {
-        return transform.rotation.eulerAngles.y;
     }
 }
